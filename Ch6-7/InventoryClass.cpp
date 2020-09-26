fd8fa80e68e589de2b6fc61eecb7ca1347718f9c
LAbeast021 <kiarashhesampour@gmail.com>
2020-09-25 22:37:48 -0700

    ch7 done

--- Ch6-7/InventoryClass.cpp
+++ Ch6-7/InventoryClass.cpp
@@ -5,94 +5,118 @@
 #include <string>
 using namespace std;
 
-class inventory
-{
-private:
-int itemNumber; //--an int that holds the items number
-int quantity; // -- an int that holds the quantity of the item on hand
-double cost; //-- a double that holds the wholesale per-unit cost of the item
-double totalCost;
-public:
-inventory()
-{
-itemNumber = 0;
-quantity = 0;
-cost = 0.0;
-totalCost= 0.0;
-}
-inventory(int in,int q,double c)
-{
-setItemNumber(in);
-setQuantity(q);
-setCost(c);
-setTotalCost();
-}
-void setItemNumber(int in)
-{
-itemNumber = in;
-}
-void setQuantity(int q)
-{
-quantity = q;
-}
-void setCost(double c)
-{
-cost = c;
-}
-void setTotalCost()
-{
-totalCost = cost*quantity;
-}
-int getItemNumber() //--returns the value in ItemNumber
-{
-return itemNumber;
-}
-int getQuantity() // - returns the value in quantity
-{
-return quantity;
-}
-double getCost() //-- returns the value in cost
-{
-return cost;
-}
-double getTotalCost()//-- computes and returns the totalCost
-{
-return cost*quantity;
-}
+class inventory{
+    private:
+        int itemNumber; 
+        int quantity; 
+        double cost; 
+        double totalCost;
+    public:
+        inventory(){
+            itemNumber = 0;
+            quantity = 0;
+            cost = 0;
+            totalCost = 0;
+        }
+    inventory(int item_number,int quantity,double cost){
+        setItemNumber(item_number);
+        setQuantity(quantity);
+        setCost(cost);
+        setTotalCost();
+    }
+    void setItemNumber(int in){
+        itemNumber = in;
+    }
+    void setQuantity(int q){
+        quantity = q;
+    }
+    void setCost(double c){
+        cost = c;
+    }
+    void setTotalCost (){
+        totalCost = cost * quantity;
+    }
+    int getItemNumber(){
+        return itemNumber;
+    }
+    int getQuantity(){
+        return quantity;
+    }
+    double getCost(){
+        return cost;
+    }
+    double getTotalCost(){
+        return cost * quantity;
+    }
 };
-int main()
-{
-int itemNumber;
-int quantity;
-double cost;
-cout << "enter item Number ";
-cin >> itemNumber;
-cout << endl;
-while(itemNumber<=0)
-{
-cout << "Invalid input.enter item Number ";
-cin >> itemNumber;
-cout << endl;
-}
-cout << "enter quantity ";
-cin >> quantity;
-cout << endl;
-while(quantity<=0)
-{
-cout << "Invalid input.enter quantity ";
-cin >> quantity;
-cout << endl;
-}
-cout << "enter cost of item ";
-cin >> cost;
-cout << endl;
-while(cost<=0)
-{
-cout << "Invalid input.enter cost of item ";
-cin >> cost;
-cout << endl;
-}
-inventory inv1(itemNumber,quantity,cost);
-cout << "Inventory total cost given by " << inv1.getTotalCost() << endl;
-return 0;
+
+int main(){
+    int item_number, quantity, user_choice;
+    double cost;
+
+    cout << " Please enter the item Number :  " ;
+    cin >> item_number;
+    if(cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Input error, Please Try again." << endl;
+    }
+    while( item_number < 0){
+        cout << "Invalid input. Please try again :  ";
+        cin >> item_number;
+
+    }
+    cout << " Please enter quantity : ";
+    cin >> quantity;
+    if(cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Input error, Please Try again." << endl;
+    }
+    while( quantity < 0){
+        cout << "Invalid input. please try again :  ";
+        cin >> quantity;
+
+    }
+    cout << " Please enter the  cost of item :  ";
+    cin >> cost;
+    if(cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Input error, Please Try again." << endl;
+    }
+    while( cost < 0){
+        cout << "Invalid input. Please try again :  ";
+        cin >> cost;
+
+    }
+    inventory current_inventory(item_number,quantity,cost);
+    cout 
+    << " Please Enter the number of one of the options : " << endl
+    << " 1 : Print the inventory ." << endl 
+    << " 2 : Calculate the total cost and display it ." << endl;
+    cin >> user_choice;
+    while (cin.fail()){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Input error, Please Try again." << endl;
+        cin >> user_choice ;
+    }
+
+    switch (user_choice){
+        case 1:
+            cout 
+            << " Item Number : " << current_inventory.getItemNumber() << endl
+            << " Item Quantity : " << current_inventory.getQuantity() << endl
+            << " Item Cost : " << current_inventory.getCost() << endl;
+            break;
+       case 2:
+            cout << " The total cost of the inventory is  $" << current_inventory.getTotalCost() << endl;
+            break;
+    
+        default:
+            cout << " Invalid option number ." << endl;
+            break;
+    } 
+    return 0;
 }
