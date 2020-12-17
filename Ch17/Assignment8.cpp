e3d1c4393a133756673c78e0d4a833179ca1323e
LAbeast021 <kiarashhesampour@gmail.com>
2020-12-16 22:04:23 -0800

    ch17

---
+++ Ch17/Assignment8.cpp
@@ -0,0 +1,10 @@
+//  Kiarash Hesampour CS216
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <cctype>
+#include <cmath>
+using namespace std;
